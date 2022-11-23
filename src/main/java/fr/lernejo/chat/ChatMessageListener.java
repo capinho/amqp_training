45cc0fc70e3d989f3b1178bdb24a7ef12f2192ba
capinho <papadiawara@hotmail.fr>
2022-11-23 12:42:03 +0100

    part3

--- src/main/java/fr/lernejo/chat/ChatMessageListener.java
+++ src/main/java/fr/lernejo/chat/ChatMessageListener.java
@@ -4,4 +4,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ChatMessageListener {
+    public void onMessage(String message) {
+        System.out.println(message);
+    }
+
 }
